9104aabfb0cdb80b441b7f52ef277f513521f8b1
yangzhiqiang <2239081756@qq.com>
2019-08-05 20:52:31 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import { Header } from 'mint-ui';
+import 'mint-ui/lib/style.css';
+Vue.component(Header.name,Header)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
