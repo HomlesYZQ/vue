f2bed86daf688d9ab9ab10ff02afe6b867d6a745
yangzhiqiang <2239081756@qq.com>
2019-08-06 08:01:14 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
   <div id="app">
-    <mt-header fixed title="固定在顶部"></mt-header>
+    <mt-header fixed title="Vue项目"></mt-header>
     <router-view/>
+    <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">消息</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">0</span></span>
+				<span class="mui-tab-label">购物车</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-search"></span>
+				<span class="mui-tab-label">设置</span>
+			</a>
+		</nav>
   </div>
 </template>
 
@@ -12,5 +30,7 @@ export default {
 </script>
 
 <style>
-
+#app{
+  padding-top: 40px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import { Header } from 'mint-ui';
 import 'mint-ui/lib/style.css';
 Vue.component(Header.name,Header)
 Vue.config.productionTip = false
-
+import '@/assets/mui/css/mui.min.css'
+import '@/assets/mui/css/icons-extra.css'
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
